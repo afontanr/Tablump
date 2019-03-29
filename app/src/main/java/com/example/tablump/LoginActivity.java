0dfa86b31b9e76f1a5b636f9988cf989042cb58d
gflorez97 <gflorez97@gmail.com>
2019-03-29 16:15:40 +0100

    commit

--- app/src/main/java/com/example/tablump/LoginActivity.java
+++ app/src/main/java/com/example/tablump/LoginActivity.java
@@ -1,10 +1,18 @@
 package com.example.tablump;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -15,8 +23,8 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Button aux = findViewById(R.id.sign_in_button);
-        aux.setOnClickListener(new View.OnClickListener() {
+        Button signin = findViewById(R.id.sign_in_button);
+        signin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
@@ -27,5 +35,15 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        Button signup = findViewById(R.id.sign_up_button);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
+

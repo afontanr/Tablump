af3818c8413f5bfab89081bbb667f5b8c3d19ecc
Guille <ocochobo@gmail.com>
2019-04-14 03:08:02 +0200

    Editar Perfil y Registro

--- app/src/main/java/com/example/tablump/EditarPerfilActivity.java
+++ app/src/main/java/com/example/tablump/EditarPerfilActivity.java
@@ -1,26 +1,23 @@
 package com.example.tablump;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-public class RegisterActivity extends AppCompatActivity {
-
-    private String username;
+public class EditarPerfilActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_editarperfil);
 
-        Button accept = findViewById(R.id.registrar_boton);
+        Button accept = findViewById(R.id.editarperfil_boton);
         accept.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getBaseContext(), PrincipalActivity.class);
-                intent.putExtra("usuario", username);
+                Intent intent = new Intent(getBaseContext(), PerfilActivity.class);
                 startActivity(intent);
             }
         });
